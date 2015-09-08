539d82aec5a06d05e7c3642604bdac4ac6530cfc
onbjerg <oliver@pfmedia.dk>
2015-09-08 09:20:33 +0200

    Add CLI ᕦ( ͡° ͜ʖ ͡°)ᕤ

---
+++ cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var lenny = require('./')
+var args = process.argv.splice(2)
+
+if (args.indexOf('-a') !== -1 || args.indexOf('--all') !== -1) {
+  lenny.faces.forEach(function (face) {
+    process.stdout.write(face + '\n')
+  })
+
+  return
+}
+
+process.stdout.write(lenny() + '\n')
