b44bf786b26df4540a5a86b462c19b3b74dbc99c
onbjerg <hi@notbjerg.me>
2016-08-02 14:41:39 +0200

    Randomly generate faces instead

--- cli.js
+++ cli.js
@@ -3,11 +3,4 @@
 var lenny = require('./')
 var args = process.argv.splice(2)
 
-if (args.indexOf('-a') !== -1 || args.indexOf('--all') !== -1) {
-  lenny.faces.forEach(function (face) {
-    process.stdout.write(face + '\n')
-  })
-  return
-}
-
 process.stdout.write(lenny())
